e2440f15b21064a1e4ec3cfa5377275bf26649f1
pank07 <ubuntu@ip-172-31-27-156.ec2.internal>
2025-05-13 08:45:22 +0000

    strt

---
+++ create_ec2.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+set -euo pipefail
+
+check_awscli() {
+	if ! command -v aws &> /dev/null; then
+		echo "AWS CLI is not installed. Please install it first." >&2
+		return 1
+	fi
+
+        }
+
+	install_awscli() {
+		echo "installing AWS CLI v2 on Linux..."
+
+		# Downlaod and install AWS CLI V2
+		curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+		sudo apt-get install -y unzip &> /dev/null
+		unzip -q awscliv2.zip
+		sudo ./aws/install
+		
+		# Verify_installation
+		aws --version
+
+		# Clean up
+		rm -rf awscliv2.zip ./aws
+	}
+
+	wait_for_instance() {
+		local insatance_id="$1"
+		echo "Waiting for instance $instance_id to be in running state..."
+
+		while true; do
+			state=$(aws ec2 describe-instances --instance-ids "instance_id" --query 'reservations[0].Instance[0].State.Name' --output text)
+			if [[ "$state" == "running" ]]; then
+				echo "instance $instance_id is now running."
+				break
+			fi 
+			sleep 10
+		done
+	}
+
+	create_ec2_instance() {
+		local ami_id="$1"
+		local instance_type="$2"
+		local key_name="$3"
+		local subnet_id="$4"
+		local security_group_ids="$5"
+		local instance_name="$6"
+
+		#Run AWS CLI command to create EC2 instance
+		instance_id=$(aws ec2 run-instances \
+			--image-id "$ami_id" \
+			--instance-type "$instance_type" \
+			--key-name "$key_name" \
+			--subnet-id "$subnet_id" \
+			--security-group-ids "$security_group_ids" \
+			--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance_name}]" \
+			--query 'Instances[0].InstanceId' \
+			--output text
+		)
+
+		if [[ -z "$instance_id" ]]; then
+			echo "Failed to create EC2 instance." >&2
+			exit 1
+		fi
+
+		echo "Instance $instance_id created successfully."
+
+		# Wait for the instance to be in runnig state
+		wait_for_instance "$instance_id"
+	}
+
+	main() {
+		if ! check_awscli ; then
+		install_awscli || exit 1
+		fi
+
+		echo "Creating EC2 instance...."
+
+		#Specify the parameters for creating the EC2 instance
+		AMI_ID="ami-0779caf41f9ba54f0"
+		INSTANCE_TYPE="t2.micro"
+		KEY_NAME="shell-script"
+		SUBNET_ID="subnet-0b5441cc4f05237bd"
+		SECURITY_GROUP_IDS="sg-0d4f238201ef49f97" # Add your security group IDs seprated by space
+		INSTANCE_NAME="Shell-Script-EC2-Demo"
+
+		# call the function to create the EC2 instance
+		
+		create_ec2_instance "$AMI_ID" "$INSTANCE_TYPE" "$KEY_NAME" "$SUBNET_ID" "$SECURITY_GROUP_IDS" "$INSTANCE_NAME"
+
+		echo "EC2 instance created successfully."
+	}
+
+	main "$@"
